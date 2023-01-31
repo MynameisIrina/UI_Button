8cc93b0caa60a04d60288dc9719e840cc592528a
MynameisIrina <chernovaira1108@gmail.com>
2023-01-31 12:19:23 +0100

    added star animation

--- Assets/Scripts/ButtonAnimation.cs
+++ Assets/Scripts/ButtonAnimation.cs
@@ -5,10 +5,15 @@ using UnityEngine.UI;
 
 public class ButtonAnimation : MonoBehaviour
 {
+    [SerializeField] private Image star1;
+    [SerializeField] private Image star2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        star1.enabled = false;
+        star2.enabled = false;
+
     }
 
     // Update is called once per frame
@@ -19,5 +24,18 @@ public class ButtonAnimation : MonoBehaviour
     public void PlayAnimation()
     {
         GetComponent<Animation>().Play("ButtonAnimation");
+        star1.enabled = true;
+        star2.enabled = true;
+        star1.GetComponent<Animation>().Play("StarAnimation");
+        star2.GetComponent<Animation>().Play("Star2Animation");
+        waitAnimation();
+
+    }
+
+    IEnumerator waitAnimation()
+    {
+        yield return new WaitForSeconds(1);
+        star1.enabled = false;
+        star2.enabled = false;
     }
 }
